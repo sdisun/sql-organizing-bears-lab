85c735a8485fb72b526e13e6fd4f2a5b8ae39f74
Neil <zooming.in@hotmail.com>
2021-07-25 20:30:48 +1000

    updates

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 1, 'M', 'black', 'chill', 1),
+('Rowdy', 1, 'M', 'blue', 'chill', 0),
+('Tabitha', 10, 'F', 'black', 'chill', 1),
+('Sergeant Brown', 3, 'M', 'black', 'angry', 1),
+('Melissa', 11, 'F', 'yellow', 'hyper', 1),
+('Grinch', 100, 'M', 'black', 'angry', 0),
+('Wendy', 9, 'F', 'pink', 'chill', 1),
+(NULL, 22, 'M', 'purple', 'angry', 1);
